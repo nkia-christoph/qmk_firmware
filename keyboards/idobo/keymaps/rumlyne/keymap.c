7c9ba254b4657de6223aced051cb3eef4edbc08c
rumlyne <ch.kroeppl@gmx.at>
2019-02-01 15:59:56 +0100

    2019-02-01

--- keyboards/idobo/keymaps/rumlyne/keymap.c
+++ keyboards/idobo/keymaps/rumlyne/keymap.c
@@ -311,6 +311,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 // Set unicode mode to Windows on startup.
+/* y tho?!
 void matrix_init_user(void) {
   set_unicode_input_mode(UC_WIN);
 };
+*/
