5ab539723c8d40b662659ac4be98b0b71d7e0956
rumlyne <ch.kroeppl@gmx.at>
2019-02-04 16:42:18 +0100

    work end

--- users/rumlyne/rules.mk
+++ users/rumlyne/rules.mk
@@ -51,7 +51,7 @@ SRC += rumlyne.c
 #EXTRAFLAGS += -flto LATER
 
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE = no       # Mouse keys(+4700)
+MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
 CONSOLE_ENABLE = no         # Console for debug(+400)
 COMMAND_ENABLE = no         # Commands for debug and configuration
@@ -60,7 +60,7 @@ NKRO_ENABLE = yes           # Nkey Rollover - if this doesn't work, see here: ht
 MIDI_ENABLE = no            # MIDI support (+2400 to 4200, depending on config)
 AUDIO_ENABLE = no           # Audio output on port C6
 UNICODE_ENABLE = yes        # Unicode
-# UNICODEMAP_ENABLE = yes        # Unicode
+# UNICODEMAP_ENABLE = yes     # Unicode
 # BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight. FUTURE TODO
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
